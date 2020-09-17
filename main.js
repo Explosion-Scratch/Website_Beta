c58cd27d3b840a3aa7697a4155649597b788b053
--Explosion-- <explosion.scratch@gmail.com>
2020-09-17 13:37:38 +0000

    Yay! The search function works!

--- main.js
+++ main.js
@@ -56,3 +56,8 @@ function DarkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+document.getElementById('Search').onsubmit = function() {
+    window.location = 'http://www.google.com/search?q=site:explosion-scratch.github.io ' + document.getElementById('Text_Search').value;
+    return false;
+}
