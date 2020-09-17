e6c760612e596ac80bf44d5a9e8043551b12aa33
--Explosion-- <explosion.scratch@gmail.com>
2020-09-17 13:39:36 +0000

    More searchbar stuff

--- main.js
+++ main.js
@@ -61,3 +61,7 @@ document.getElementById('Search').onsubmit = function() {
     window.location = 'http://www.google.com/search?q=site:explosion-scratch.github.io ' + document.getElementById('Text_Search').value;
     return false;
 }
+document.getElementById('Mobile_Search').onsubmit = function() {
+    window.location = 'http://www.google.com/search?q=site:explosion-scratch.github.io ' + document.getElementById('Mobile_Text_Search').value;
+    return false;
+}
