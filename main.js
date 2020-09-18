d83795591b1cd2cdb1603f4b0de3eb9e26777ec8
--Explosion-- <explosion.scratch@gmail.com>
2020-09-18 20:28:49 +0000

    Hmm, it's laggy?

--- main.js
+++ main.js
@@ -65,3 +65,29 @@ document.getElementById('Mobile_Search').onsubmit = function() {
     window.location = 'http://www.google.com/search?q=site:explosionscratc.repl.co ' + document.getElementById('Mobile_Text_Search').value;
     return false;
 }
+function includeHTML() {
+	var z, i, elmnt, file, xhttp;
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+		file = elmnt.getAttribute("include-html");
+		if (file) {
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function () {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						elmnt.innerHTML = this.responseText;
+					}
+					if (this.status == 404) {
+						elmnt.innerHTML = "Page not found.";
+					}
+					elmnt.removeAttribute("include-html");
+					includeHTML();
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			return;
+		}
+	}
+};
