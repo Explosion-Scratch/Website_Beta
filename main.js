99240c81a4e71c18ab4b99f8b4a3b74ed8f13899
--Explosion-- <explosion.scratch@gmail.com>
2020-09-20 18:01:20 +0000

    Beautified JS

--- main.js
+++ main.js
@@ -15,56 +15,57 @@ const searchBoxDesktop = document.querySelector(".searchbox-desktop");
 
 // Scroll
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function() {
 
-  if (window.pageYOffset > 300) {
-    header.classList.add("scrolled");
-    headerContainer.classList.add("fixed");
-  } else {
-    header.classList.remove("scrolled");
-    headerContainer.classList.remove("fixed");
-  }
+	if (window.pageYOffset > 300) {
+		header.classList.add("scrolled");
+		headerContainer.classList.add("fixed");
+	} else {
+		header.classList.remove("scrolled");
+		headerContainer.classList.remove("fixed");
+	}
 })
 
 // Desktop Search
 
-searchIconDesktop.addEventListener("click", function () {
-  searchBoxDesktop.classList.toggle("active");
+searchIconDesktop.addEventListener("click", function() {
+	searchBoxDesktop.classList.toggle("active");
 })
 
 // Mobile Menu 
 
-menuIcon.addEventListener("click", function () {
-  mobileMenuItems.classList.toggle("active");
+menuIcon.addEventListener("click", function() {
+	mobileMenuItems.classList.toggle("active");
 })
 
 // Mobile Search
 
-mobileSearchIcon.addEventListener("click", function () {
-  searchBoxMobile.classList.toggle("active");
+mobileSearchIcon.addEventListener("click", function() {
+	searchBoxMobile.classList.toggle("active");
 
-  if (searchBoxMobile.classList.contains("active")) {
-    searchIcon.style.display = "none";
-    timesIcon.style.display = "block";
-  } else {
-    searchIcon.style.display = "block";
-    timesIcon.style.display = "none";
-  }
+	if (searchBoxMobile.classList.contains("active")) {
+		searchIcon.style.display = "none";
+		timesIcon.style.display = "block";
+	} else {
+		searchIcon.style.display = "block";
+		timesIcon.style.display = "none";
+	}
 })
 
 function DarkMode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+	var element = document.body;
+	element.classList.toggle("dark-mode");
 }
 
 document.getElementById('Search').onsubmit = function() {
-    window.location = 'http://www.google.com/search?q=site:explosionscratc.repl.co ' + document.getElementById('Text_Search').value;
-    return false;
+	window.location = 'http://www.google.com/search?q=site:explosionscratc.repl.co ' + document.getElementById('Text_Search').value;
+	return false;
 }
 document.getElementById('Mobile_Search').onsubmit = function() {
-    window.location = 'http://www.google.com/search?q=site:explosionscratc.repl.co ' + document.getElementById('Mobile_Text_Search').value;
-    return false;
+	window.location = 'http://www.google.com/search?q=site:explosionscratc.repl.co ' + document.getElementById('Mobile_Text_Search').value;
+	return false;
 }
+
 function menu(x) {
-  x.classList.toggle("menu");
+	x.classList.toggle("menu");
 }
